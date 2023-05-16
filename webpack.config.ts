7b0624a1096536506c9710fca3b7253ae6cb2330
Greg Richardson <greg.nmr@gmail.com>
2023-05-16 17:38:03 -0600

    feat: working pg poc

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,4 @@
-import { Configuration } from 'webpack';
+import { Configuration, DefinePlugin, ProvidePlugin } from 'webpack';
 import { resolve } from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
@@ -29,13 +29,38 @@ const config: Configuration = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      'pg-native': false,
+      'pg-cloudflare': false,
+    },
     fallback: {
       net: resolve(__dirname, 'src/polyfill'),
+      path: require.resolve('path-browserify'),
+      fs: false,
+      stream: false,
+      util: false,
+      tls: false,
+      dns: false,
+      crypto: false,
+      assert: false,
     },
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'TCP/IP Demo',
+    }),
+    new DefinePlugin({
+      process: {
+        env: {},
+        nextTick: DefinePlugin.runtimeValue(function () {
+          return function (fn: () => void) {
+            setTimeout(fn, 0);
+          };
+        }, []),
+      },
+    }),
+    new ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
     }),
   ],
   output: {
