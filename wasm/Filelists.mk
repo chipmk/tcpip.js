abe7bf07f488685f101837b0a0c2dbfa6ae7db6b
Greg Richardson <greg.nmr@gmail.com>
2024-11-04 11:55:50 -0700

    refactor: tap interface into own c file

--- wasm/Filelists.mk
+++ wasm/Filelists.mk
@@ -1,3 +1,4 @@
 SRC_FILES= \
 	$(SRC_DIR)/stack.c \
+	$(SRC_DIR)/tap_interface.c \
 	$(SRC_DIR)/arch.c
--- wasm/include/lwipopts.h
+++ wasm/include/lwipopts.h
@@ -4,7 +4,20 @@
 #define NO_SYS 1
 #define SYS_LIGHTWEIGHT_PROT 0
 
+#define LWIP_NETIF_LOOPBACK 1
+#define LWIP_HAVE_LOOPIF 1
+
+#define LWIP_ARP 1
+
+#define IP_FORWARD 1
+
+#define LWIP_IPV4 1
+#define LWIP_ICMP 1
+
 #define LWIP_SOCKET 0
 #define LWIP_NETCONN 0
+#define LWIP_NETIF_API 0
+
+#define LWIP_TCP 1
 
 #endif /* LWIPOPTS_H */
