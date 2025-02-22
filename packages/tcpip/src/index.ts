8bfab71ceda7f8c8e8d74b1e9babb523fe6520a5
Greg Richardson <greg.nmr@gmail.com>
2025-02-22 14:41:20 -0700

    fix: index exports

--- packages/tcpip/src/index.ts
+++ packages/tcpip/src/index.ts
@@ -1,25 +1,31 @@
 export { createStack, type NetworkStack } from './stack.js';
 export type { NetworkInterface } from './types.js';
 
-export { type LoopbackInterfaceOptions } from './bindings/loopback-interface.js';
-export type { LoopbackInterface } from './bindings/loopback-interface.js';
+export type {
+  LoopbackInterface,
+  LoopbackInterfaceOptions,
+} from './bindings/loopback-interface.js';
 
-export { type TunInterfaceOptions } from './bindings/tun-interface.js';
-export type { TunInterface } from './bindings/tun-interface.js';
+export type {
+  TunInterface,
+  TunInterfaceOptions,
+} from './bindings/tun-interface.js';
 
-export {
-  VirtualTapInterface as TapInterface,
-  type TapInterfaceOptions,
+export type {
+  TapInterface,
+  TapInterfaceOptions,
 } from './bindings/tap-interface.js';
 
-export { type BridgeInterfaceOptions } from './bindings/bridge-interface.js';
-export type { BridgeInterface } from './bindings/bridge-interface.js';
+export type {
+  BridgeInterface,
+  BridgeInterfaceOptions,
+} from './bindings/bridge-interface.js';
 
-export {
-  type TcpConnectionOptions,
-  type TcpListenerOptions,
+export type {
+  TcpConnectionOptions,
+  TcpListenerOptions,
 } from './bindings/tcp.js';
 export type { TcpConnection, TcpListener } from './bindings/tcp.js';
 
-export { type UdpDatagram, type UdpSocketOptions } from './bindings/udp.js';
+export type { UdpDatagram, UdpSocketOptions } from './bindings/udp.js';
 export type { UdpSocket } from './bindings/udp.js';
